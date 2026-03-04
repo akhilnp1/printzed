550dae226b0bded8920242f186ec6a6c3eb10890
akhilnp <npakhil01@gmail.com>
2026-03-05 02:04:03 +0530

    Fix allowed hosts

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -6,8 +6,12 @@ SECRET_KEY = 'django-insecure-change-this-in-production-xyz123'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = [
+    'printzed-backend.onrender.com',
+    'printzed-project.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
