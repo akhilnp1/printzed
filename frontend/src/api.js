7d4dc5cf1d8228524a79be92b62b6088d279b078
akhilnp <npakhil01@gmail.com>
2026-03-05 03:16:15 +0530

    added redirects file in frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000/api',
 });
 
+// export const MEDIA_URL = process.env.REACT_APP_MEDIA_URL || 'http://localhost:8000';
 export const MEDIA_URL = '';
 
 // Services
